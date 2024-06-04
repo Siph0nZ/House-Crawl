4b8010fbffe521fddd18aa48231fdb3d9e12f4ce
Mason Garrod <151958427+Siph0nZ@users.noreply.github.com>
2024-06-04 11:57:44 +0100

    WIP making corridors bigger

--- Assets/Scripts/CorridorFirstRoomGenerator.cs
+++ Assets/Scripts/CorridorFirstRoomGenerator.cs
@@ -22,7 +22,7 @@ public class CorridorFirstRoomGenerator : MapGenerator
     {
         HashSet<Vector2Int> floorPositions = new HashSet<Vector2Int>();
         HashSet<Vector2Int> potentialRoomPositions = new HashSet<Vector2Int>();
-        CreateCorridors(floorPositions, potentialRoomPositions);
+        List<List<Vector2Int>> corridors = CreateCorridors(floorPositions, potentialRoomPositions);
 
         HashSet<Vector2Int> roomPositions = CreateRooms(potentialRoomPositions);
 
@@ -33,6 +33,12 @@ public class CorridorFirstRoomGenerator : MapGenerator
         CreateRoomsAtDeadEnd(deadEnds, roomPositions);
         
         floorPositions.UnionWith(roomPositions);
+
+        for (int i = 0; i < corridors.Count; i++)
+        {
+            corridors[i] = IncreaseCorridorSizeByOne(corridors[i]);
+            floorPositions.UnionWith(corridors[i]);
+        }
 
         tilemapVisualizer.PlaceFloorTiles(floorPositions);
         WallGenerator.CreateWalls(floorPositions, tilemapVisualizer);
@@ -93,18 +99,73 @@ public class CorridorFirstRoomGenerator : MapGenerator
         return roomPositions;
     }
 
-    private void CreateCorridors(HashSet<Vector2Int> floorPositions, HashSet<Vector2Int> potentialRoomPositions)
+    private List<List<Vector2Int>> CreateCorridors(HashSet<Vector2Int> floorPositions, HashSet<Vector2Int> potentialRoomPositions)
     {
         var currentPosition = startPosition; 
         potentialRoomPositions.Add(currentPosition);
+        List<List<Vector2Int>> corridors = new List<List<Vector2Int>>();
 
         for (int i = 0; i < corridorCount; i++)
         {
             var corridor = ProceduralGeneration.RandomWalkCorridor(currentPosition, corridorLength); // generates corridor 
+            corridors.Add(corridor);
             currentPosition = corridor[corridor.Count - 1]; // sets next current position to the last position of the corridor, keeps them connected
             potentialRoomPositions.Add(currentPosition);
             floorPositions.UnionWith(corridor);
         }
+        return corridors;
+    }
+
+    public List<Vector2Int> IncreaseCorridorSizeByOne(List<Vector2Int> corridor)
+    {
+        List<Vector2Int> newCorridor = new List<Vector2Int>();
+        Vector2Int previousDirection = Vector2Int.zero;
+
+        for (int i = 0; i < corridor.Count; i++)
+        {
+            Vector2Int directionFromCell = corridor[i] - corridor[i - 1];
+            if (previousDirection != Vector2Int.zero && directionFromCell != previousDirection)
+            {
+                for (int x = -1; x < 2; x++)
+                {
+                    for (int y = -1; x < 2; y++)
+                    {
+                        newCorridor.Add(corridor[i - 1] + new Vector2Int(x, y));
+                    }
+                }
+                previousDirection = directionFromCell; // saves new direction
+            }
+
+            else
+            {
+                // Adds a single cell in direction of 90 degrees
+                Vector2Int newCorridorTileOffset = GetDirection90From(directionFromCell); // rotates direction to find nearby cell
+                newCorridor.Add(corridor[i - 1]);
+                newCorridor.Add(corridor[i - 1] + newCorridorTileOffset);
+            }
+        }
+        return newCorridor;
+    }
+
+    private Vector2Int GetDirection90From(Vector2Int direction)
+    {
+        if (direction == Vector2Int.up)
+        {
+            return Vector2Int.right;
+        }
+        if (direction == Vector2Int.right)
+        {
+            return Vector2Int.down;
+        }
+        if (direction == Vector2Int.down)
+        {
+            return Vector2Int.left;
+        }
+        if (direction == Vector2Int.left)
+        {
+            return Vector2Int.up;
+        }
+        return Vector2Int.zero;
     }
 }
 
